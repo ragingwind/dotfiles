92dc494dbe522ece7b8a6d84fb346a717be9b25d
ragingwind <ragingwind@gmail.com>
2013-08-16 10:34:25 +0900

    Update subl

--- sublime/functions.zsh
+++ sublime/functions.zsh
@@ -1,3 +1,10 @@
 subl() {
-  /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl $1 &
+
+ if [[ "$(uname -s)" == "Darwin" ]]
+    then
+       	/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl $1 &
+ 	else
+		/usr/local/bin/Sublime\ Text\ 2/sublime_text $1 &
+	fi
+
 }
