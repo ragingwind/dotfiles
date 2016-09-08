999ba89993814afb842a37809ebab35347668db8
ragingwind <ragingwind@gmail.com>
2016-09-08 11:49:18 +0900

    Add more tools thumnail, videocut for 30min

--- tools/aliase.zsh
+++ tools/aliase.zsh
@@ -1,1 +1,3 @@
-alias screenshot="convert -append -adaptive-resize 1024 *.png o.png"
+alias screenshot="convert -append -adaptive-resize 1984 $*"
+alias thumbnail="montage -frame 1 -geometry 1980 -background white $*"
+alias screencut30 = "time ffmpeg -v quiet -y -i $1 -vcodec copy -acodec copy -ss 00:00:00 -t 00:30:00 -sn $2"
