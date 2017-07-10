3f546da00ac9cbdf052770cf78fb41a3e2e07453
Jimmy Moin <ragingwind@gmail.com>
2017-07-10 16:33:40 +0900

    shell: add more alias for screenshot, top

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -1,3 +1,7 @@
 alias screenshot="convert -append -adaptive-resize 1984 $*"
+alias screenshot1024="convert -append -adaptive-resize 1024 $*"
+alias screenshot640="convert -append -adaptive-resize 640 $*"
+alias screenshot320="convert -append -adaptive-resize 320 $*"
 alias thumbnail="montage -frame 1 -geometry 1024 -background white $*"
 alias screencut30="time ffmpeg -v quiet -y -i $1 -vcodec copy -acodec copy -ss 00:00:00 -t 00:30:00 -sn $2"
+alias top="top -o cpu"
