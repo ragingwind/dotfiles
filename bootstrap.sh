6cba6f93994f7ae5193ff2848359a45cb5f3930c
Jimmy Moon <ragingwind@gmail.com>
2020-11-30 19:42:01 +0900

    Fix permmsion and running error

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,13 +15,15 @@ if test ! -d ~/.nvm; then
   (
     git clone https://github.com/creationix/nvm.git "$NVM_DIR"
     cd "$NVM_DIR"
-    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+    git switch `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
   ) && \. "$NVM_DIR/nvm.sh"
 
   echo '
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 ' >> $HOME/.profile
+
+  source $HOME/.profile
 
   # install node lts version as default
   nvm install --lts
@@ -44,4 +46,11 @@ if [ "$(uname -s)" == "Darwin" ]; then
   brew bundle
 fi
 
-source $HOME/.profile
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+pip3 install pynvim
+
+# Set permission for zsh
+sudo chmod -R 755 /usr/local/share/zsh
+sudo chown -R root:staff /usr/local/share/zsh
